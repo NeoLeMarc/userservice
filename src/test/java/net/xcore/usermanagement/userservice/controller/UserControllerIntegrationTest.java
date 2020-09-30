c1edaa870330e3b712b3a3740840c2f1da9f302c
Marcel Noe <marcel@marcel-noe.de>
2020-09-30 10:07:27 +0200

    Proceeding with Integration Tests for User Service.

--- src/test/java/net/xcore/usermanagement/userservice/controller/UserControllerIntegrationTest.java
+++ src/test/java/net/xcore/usermanagement/userservice/controller/UserControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package net.xcore.usermanagement.userservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import net.xcore.usermanagement.userservice.domain.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,14 +44,41 @@ public class UserControllerIntegrationTest {
 
   @Test
   @WithMockUser
-  public void normalUserCanAccessHelloUserMethod() throws Exception {
-    mockMvc.perform(get("/hellouser").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+  public void normalUserCanAccessNormalUserRequests() throws Exception {
+    performNormalUserRequests();
   }
 
+  private void performNormalUserRequests() throws Exception {
+    mockMvc.perform(get("/hellouser").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+    mockMvc.perform(get("/user/testuser").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk());
+    mockMvc.perform(post("/user/verify").contentType(MediaType.APPLICATION_FORM_URLENCODED)
+        .param("username", "foo")
+        .param("password", "bar"))
+        .andExpect(status().isOk());
+    mockMvc.perform((get("/user/testuser").contentType(MediaType.APPLICATION_JSON)))
+        .andExpect(status().isOk());
+  }
+
+  @SuppressWarnings("OverlyBroadThrowsClause")
   @Test
   @WithMockUser
-  public void normalUserCanNotAccessHelloAdminMethod() throws Exception {
+  public void normalUserCanNotAccessHelloAdminRequests() throws Exception {
+    performAdminRequests();
+  }
+
+  private void performAdminRequests() throws Exception {
     mockMvc.perform(get("/helloadmin").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isForbidden());
+    performPostUserRequest();
+  }
+
+  private void performPostUserRequest() throws Exception {
+    ObjectMapper mapper = new ObjectMapper();
+    User testUser = new User();
+    String jsonUser = mapper.writeValueAsString(testUser);
+    mockMvc.perform(post("/user").contentType(MediaType.APPLICATION_JSON).content(jsonUser)
+    ).andExpect(status().isForbidden());
   }
 
 
