e3d41a1a4954fbee0b0bab6fbd9408728cf2a3fe
Marcel Noe <marcel@marcel-noe.de>
2020-10-01 20:20:54 +0200

    unit tests work - but need to fix unnecessary stubbing exception

--- src/test/java/net/xcore/usermanagement/userservice/util/CustomAuthenticationProviderUnitTest.java
+++ src/test/java/net/xcore/usermanagement/userservice/util/CustomAuthenticationProviderUnitTest.java
@@ -1,10 +1,14 @@
 package net.xcore.usermanagement.userservice.util;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Collection;
 import java.util.Optional;
+import javax.annotation.PostConstruct;
 import net.xcore.usermanagement.userservice.domain.User;
 import net.xcore.usermanagement.userservice.service.UserService;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -12,6 +16,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -23,6 +28,9 @@ public class CustomAuthenticationProviderUnitTest {
   public static final String ADMIN_ROLE = "admin";
   public static final String ADMIN_USERNAME = "adminusername";
   public static final String ADMIN_PASSWORD = "adminpassword";
+  public static final String WRONG_USERNAME = "wrongusername";
+  public static final String WRONG_PASSWORD = "wrongpassword";
+
   private CustomAuthenticationProvider customAuthenticationProvider;
 
   @Mock
@@ -41,19 +49,54 @@ public class CustomAuthenticationProviderUnitTest {
     user.setPassword(USER_PASSWORD);
 
     adminUser = new User();
-    user.setRole(ADMIN_ROLE);
-    user.setUsername(ADMIN_USERNAME);
-    user.setPassword(ADMIN_PASSWORD);
+    adminUser.setRole(ADMIN_ROLE);
+    adminUser.setUsername(ADMIN_USERNAME);
+    adminUser.setPassword(ADMIN_PASSWORD);
 
     Mockito.when(mockUserService.getUser(Mockito.eq(USER_USERNAME))).thenReturn(Optional.of(user));
     Mockito.when(mockUserService.getUser(Mockito.eq(ADMIN_USERNAME))).thenReturn(Optional.of(adminUser));
-    Mockito.when(mockUserService.verifyUserPassword(Mockito.eq(USER_USERNAME), Mockito.eq(USER_PASSWORD))).thenReturn(Optional.of(user));
     Mockito.when(mockUserService.verifyUserPassword(Mockito.eq(ADMIN_USERNAME), Mockito.eq(ADMIN_PASSWORD))).thenReturn(Optional.of(adminUser));
+    Mockito.when(mockUserService.verifyUserPassword(Mockito.eq(USER_USERNAME), Mockito.eq(USER_PASSWORD))).thenReturn(Optional.of(user));
   }
 
   @Test
-  public void testAdminAuth(){
+  public void testNormalUserIsVerifiedAndAssignedOnlyUserRole(){
     Authentication authentication = new PreAuthenticatedAuthenticationToken(USER_USERNAME, USER_PASSWORD);
-    customAuthenticationProvider.authenticate(authentication);
+    Authentication token = customAuthenticationProvider.authenticate(authentication);
+    Mockito.verify(mockUserService, Mockito.times(1)).verifyUserPassword(USER_USERNAME, USER_PASSWORD);
+
+    assertThat(token.isAuthenticated()).isTrue();
+    Collection<? extends GrantedAuthority> authorities = token.getAuthorities();
+    assertThat(authorities.size()).isEqualTo(1);
+    assertThat(authorities.contains(new SimpleGrantedAuthority("USER")));
+  }
+
+  @Test
+  public void testAdminUserIsVerifiedAndAssignedAdminAndUserRole(){
+    Authentication authentication = new PreAuthenticatedAuthenticationToken(ADMIN_USERNAME, ADMIN_PASSWORD);
+    Authentication token = customAuthenticationProvider.authenticate(authentication);
+    Mockito.verify(mockUserService, Mockito.times(1)).verifyUserPassword(ADMIN_USERNAME, ADMIN_PASSWORD);
+
+    assertThat(token.isAuthenticated()).isTrue();
+    Collection<? extends GrantedAuthority> authorities = token.getAuthorities();
+    assertThat(authorities.size()).isEqualTo(2);
+    assertThat(authorities.contains(new SimpleGrantedAuthority("USER")));
+    assertThat(authorities.contains(new SimpleGrantedAuthority("ADMIN")));
+  }
+
+  @Test
+  public void testUserWithIncorrectPasswordIsNotVerified(){
+    Authentication authentication = new PreAuthenticatedAuthenticationToken(ADMIN_USERNAME, WRONG_PASSWORD);
+    Authentication token = customAuthenticationProvider.authenticate(authentication);
+    Mockito.verify(mockUserService, Mockito.times(1)).verifyUserPassword(ADMIN_USERNAME, WRONG_PASSWORD);
+    assertThat(token).isNull();
+  }
+
+  @Test
+  public void testNotExistingUserWitCorrectPasswordIsNotVerified(){
+    Authentication authentication = new PreAuthenticatedAuthenticationToken(WRONG_USERNAME, ADMIN_PASSWORD);
+    Authentication token = customAuthenticationProvider.authenticate(authentication);
+    Mockito.verify(mockUserService, Mockito.times(1)).verifyUserPassword(WRONG_USERNAME, ADMIN_PASSWORD);
+    assertThat(token).isNull();
   }
 }
